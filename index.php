d52f8633b31891a7c0f4ed1612f0cda2374632a2
Gonzalo Zúñiga <astaroth_x3m@hotmail.com>
2016-05-31 13:15:56 -0300

    Inicio del diseño, creacion de los CSS.

--- index.php
+++ index.php
@@ -1,36 +1,48 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="estilos/estilosIndex.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta charset="utf-8">
-    <title>inicio</title>
+    <title>Inicio de Sesión</title>
   </head>
   <body>
-    <h1>Iniciar Sesión</h1>
-        <form action="controlador/validar.php" method="post">
-            <input
-                type="email"
-                name="txtCorreo"
-                placeholder="Escriba su correo:"
-                required="required"
-            >
-            <br>
-            <input
-                type="password"
-                name="txtPass"
-                placeholder="Escriba su pass:"
-                required="required"
-            >
-            <br>
-            <input
-                type="submit"
-                name="btnIniciar"
-                value="Iniciar sesión"
-            >
-        </form>
-        <div id="subTitulos">
-          si no esta registrado entrar
-        </div>
-        <a href="registro.php">aqui</a>
+  <div class="banner">
+    <img src="img/banner2.jpg">    
+  </div>
+    
+    
+  <div class="panelForm">
+    <div class="divForm">
+      <h2>Iniciar Sesión</h2>
+      <form action="controlador/validar.php" method="post">
+          <legend>Hola</legend>
+          <input
+              type="email"
+              name="txtCorreo"
+              placeholder="Escriba su correo:"
+              required="required"
+          >
+          <br>
+          <input
+              type="password"
+              name="txtPass"
+              placeholder="Escriba su pass:"
+              required="required"
+          >
+          <br>
+          <input class="boton" 
+              type="submit"
+              name="btnIniciar"
+              value="Iniciar sesión"
+          >
+      </form>
+      <div id="subTitulos">
+        ¿Aún no creas tu cuenta?
+      </div>
+      <a href="registro.php">Registrarse</a>
+    </div>
+  </div>
 
   </body>
 </html>
---
+++ inicio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilos/estilosInicio.css">
+	<meta charset="utf-8">
+	<title>Bienvenido a PetVet</title>
+</head>
+<body>
+	<img src="">
+
+</body>
+</html>
