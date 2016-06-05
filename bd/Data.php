86b3a8c048d13fdb9883218c950730ee82195bfe
D-loboli <lobolinkinxxx@gmail.com>
2016-06-05 12:55:02 -0400

    actalisaciones de pia

--- bd/Data.php
+++ bd/Data.php
@@ -1,6 +1,4 @@
-
 <?php
-
 require_once "Conexion.php";
 
 class Data{
@@ -9,68 +7,197 @@ class Data{
     public function __construct(){
         $this->c = new Conexion(
             "localhost",
-            "sesiones",
+            "veterinaria",
             "root",
-            "123456"
+            ""
         );
     }
 
-    public function crearUsuarioBasico($nombre, $apellido, $rut, $clave,$tipoAcceso){
-        $q = "insert into usuario
-            values (null,
-                '$correo',
-                '$apellido',
+    public function getAcceso($rut, $clave){
+        $query = "select a.id ".
+        "from tipoAcceso a, usuario u ".
+        "where a.id = u.tipoAcceso and ".
+        "u.rut = '$rut' and ".
+        "u.clave = '$clave';";
+
+        $rs = $this->c->ejecutar($query);
+
+        $tipoAcceso = 0;
+
+        /*Si existe algún registro*/
+        if($reg = mysql_fetch_array($rs)){
+            $tipoAcceso = $reg[0];
+        }
+
+        return $tipoAcceso;
+    }
+
+    public function listarMascotasId($idMa, $idMe){
+        $q = "select m.nombre as 'mascota', m.edad, m.raza, m.sexo, m.vacunas, me.nombre 'medico', a.id 'numero atencion'
+	           from mascota m, medico me, atencion a
+               where m.id = $idMa and me.id = $idMe;";
+
+        $rs = $this->c->ejecutar($q);
+        echo "<h1>Listado de mascotas</h1>";
+
+        echo "<table border='1'>";
+            echo "<tr>";
+                echo "<th>Nombre</th>";
+                echo "<th>Edad</th>";
+                echo "<th>Raza</th>";
+                echo "<th>Sexo</th>";
+                echo "<th>Vacunas</th>";
+                echo "<th>Medico</th>";
+                echo "<th>Numero Atencion</th>";
+            echo "</tr>";
+
+        while($reg = mysql_fetch_array($rs)){
+            echo "<tr>";
+                echo "<td>$reg[0]</td>";
+                echo "<td>$reg[1]</td>";
+                echo "<td>$reg[2]</td>";
+                echo "<td>$reg[3]</td>";
+                echo "<td>$reg[4]</td>";
+                echo "<td>$reg[5]</td>";
+                echo "<td>$reg[6]</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    }
+
+    public function eliminarPropietario($id){
+        $q = "delete from propietario where id = $id";
+        $this->c->ejecutar($q);
+    }
+
+    public function getNombrePropietario($rut){
+        $q = "select nombre
+                from propietario
+                where rut = '$rut';";
+
+        $rs = $this->c->ejecutar($q);
+
+        $nombre = "null";
+        if($reg = mysql_fetch_array($rs)){
+            $nombre = $reg[0];
+        }
+
+        return $nombre;
+    }
+
+    public function getNombreMedico($rut){
+        $q = "select nombre
+                from medico
+                where rut = '$rut';";
+
+        $rs = $this->c->ejecutar($q);
+
+        $nombre = "null";
+        if($reg = mysql_fetch_array($rs)){
+            $nombre = $reg[0];
+        }
+
+        return $nombre;
+    }
+
+    public function crearPropietario($rut, $nombre, $apellido, $telefono){
+        $q = " insert into propietario values (null,
                 '$rut',
-                '$clave',
-                '$tipoAcceso');";
+                '$nombre',
+                '$apellido',
+                 $telefono);";
 
         $this->c->ejecutar($q);
     }
 
-    public function cargarTipoAcceso(){
-        $q = "select * from tipoAcceso";
+    public function crearMedico($rut, $nombre, $apellido){
+        $q = "insert into medico values (null,
+                '$rut',
+                '$nombre',
+                '$apellido');";
+
+        $this->c->ejecutar($q);
+    }
+
+    public function crearMascota($nombre,$edad, $raza, $sexo, $vacunas, $propietario){
+        $q = " insert into mascota values (null,
+                    '$nombre',
+                     $edad,
+                    '$raza',
+                    '$sexo',
+                    $vacunas,
+                    $propietario);";
+
+        $this->c->ejecutar($q);
+    }
+
+
+
+    public function formularioAgregarPropietario($action){
+        echo "<h3>Registrarse</h3>";
+        echo "<form action='$action' method='post'>";
+            echo "<input type='text' name='txtNombre' placeholder='Nombre:'>";
+            echo "<br/>";
+            echo "<input type='text' name='txtApellido' placeholder='Apellido:'>";
+            echo "<br/>";
+            echo "<input type='password' name='txtClave' placeholder='Clave:'>";
+            echo "<br/>";
+            echo "<input type='text' name='txtTelefono' placeholder='Telefono:'>";
+            echo "<br/>";
+            echo "<input type='submit' name='btnRegistrar' value='Registrar'>";
+        echo "</form>";
+    }
+
+    public function formularioAgregarMedico($action){
+        echo "<h3>Registrar medico</h3>";
+        echo "<form action='$action' method='post'>";
+            echo "<input type='text' name='txtNombre' placeholder='Nombre:'>";
+            echo "<br/>";
+            echo "<input type='text' name='txtApellido' placeholder='Apellido:'>";
+            echo "<br/>";
+            echo "<input type='password' name='txtClave' placeholder='Clave:'>";
+            echo "<br/>";
+            echo "<input type='submit' name='btnRegistrar' value='Registrar'>";
+        echo "</form>";
+    }
+
+    public function cargarPropietarios(){
+        $q = "select * from propietario";
 
         $rs = $this->c->ejecutar($q);
 
-        echo "<select name='cboPrivilegio'>";
+        echo "<select name='cboPropietario'>";
         while($reg = mysql_fetch_array($rs)){
             $id = $reg[0];
-            $acceso = $reg[1];
-            echo "<option value='$id'>$acceso</option>";
-
+            $nombre = $reg[1];
+            echo "<option value='$id'>$nombre</option>";
         }
         echo "</select>";
-      }
+    }
 
-      public function formularioCrear($action){
-          echo "<h3>Crear usuario</h3>";
-          echo "<form action='$action' method='post'>";
-              echo "<input type='text' name='txtNombre' placeholder='Nombre:'>";
-              echo "<br/>";
-              echo "<input type='text' name='txtApellido' placeholder='Appellido:'>";
-              echo "<br/>";
-              echo "<input type='nuber' name='txtRut' placeholder='Rut:'>";
-              echo "<br/>";
-              echo "<input type='password' name='txtClave' placeholder='Clave:'>";
-              echo "<br/>";
-              $this->cargarTipoAcceso();
-              echo "<br/>";
-              echo "<input type='submit' name='btnRegistrar' value='Registrar'>";
-          echo "</form>";
-
-          /*
-          if(isset($_POST["btnRegistrar"])){
-          require_once "bd/Data.php";
-
-          $nombre = $_POST["txtNombre"];
-          $apellido = $_POST["txtApellido"];
-          $rut = $_POST["txtRut"];
-          $clave = $_POST["txtClave"];
-          $tipoAcceso = $_POST["txtTipoAcceso"];
-          $privilegio = $_POST["cboPrivilegio"];
-
-          $d = new Data();
-          $d->crearUsuarioBasico($nombre, $apellido, $rut, $clave,$tipoAcceso);
-          */
-      }
- ?>
+    public function formularioAgregarMascota($action){
+        echo "<h3>Agregar Mascota</h3>";
+        echo "<form action='$action' method='post'>";
+            echo "<input type='text' name='txtNombre' placeholder='Nombre:'>";
+            echo "<br/>";
+            echo "<input type='text' name='txtApellido' placeholder='Edad:'>";
+            echo "<br/>";
+            echo "<input type='password' name='txtClave' placeholder='Raza:'>";
+            echo "<br/>";
+            echo "<input type='text' name='txtTelefono' placeholder='Vacunas:'>";
+            echo "<br/>";
+            echo "Sexo:";
+            echo "<br/>";
+            echo "<input type="radio" name="rdSexo" value="Masculino">Masculino.";
+            echo "<input type="radio" name="rdSexo" value="Femenino">Femenino.";
+            echo "<br/>";
+            echo "<input type='text' name='txtTelefono' placeholder='Vacunas:'>";
+            echo "<br/>";
+            cargarPropietarios();
+            echo "<br/>";
+            echo "<input type='submit' name='btnRegistrar' value='Registrar'>";
+        echo "</form>";
+    }
+}
+?>
--- script.sql
+++ script.sql
@@ -1,44 +1,88 @@
 create database veterinaria;
  use veterinaria;
 
- create table usuario(
-     id auto_increment,
-     nombre varchar (20),
-     apellido varchar (20),
-     rut varchar (12),
-     clave varchar (25),
-     tipoAcceso int,
-     primary key (id),
-     foreign key (tipoAcceso) references tipoAcceso(id)
- );
-
- insert into usuario values (null,'Juan','Gomez','111-1','123456',1);
- insert into usuario values (null,'Juana','Rojas','222-2','654321',2);
- insert into usuario values (null,'Camila','Gonzalez','333-3','123456',2);
- insert into usuario values (null,'Valeria','Flores','444-4','654321',2);
- insert into usuario values (null,'Pedro','Fernandez','555-5','123456',2);
- insert into usuario values (null,'Joaquin','Olivares','666-6','654321',2);
- insert into usuario values (null,'Mauricio','Castillo','777-7','123456',2);
- insert into usuario values (null,'Isidora','Castro','888-8','654321',2);
- insert into usuario values (null,'Dania','Pereira','999-9','123456',2);
-
  create table tipoAcceso(
-     id auto_increment,
+     id int auto_increment,
      acceso varchar (20),
      primary key (id)
  );
+
 insert into tipoAcceso values (null,'Administrador');
 insert into tipoAcceso values (null,'Medico Veterinario');
 insert into tipoAcceso values (null,'Dueño');
 
+create table propietario(
+id int auto_increment,
+rut varchar (12),
+nombre varchar (20),
+apellido varchar (20),
+telefono int,
+primary key (id)
+);
+
+ insert into propietario values (null,'101-0','Maria Jose','Valdes',778899);
+
  create table mascota(
-     id auto_increment,
+     id int auto_increment,
      nombre varchar (25),
      edad int,
      raza varchar (25),
      sexo varchar(15),
      vacunas int,
-     medico int,
+     propietario int,
      primary key (id),
-     foreign key (medico) references usuario(id)
+     foreign key (propietario) references propietario(id)
  );
+
+ insert into mascota values (null,'boby',5,'Labrador','macho',2,1);
+ insert into mascota values (null,'bobo',6,'dalmata','macho',6,1);
+
+ create table medico(
+id int auto_increment,
+rut varchar (12),
+nombre varchar (20),
+apellido varchar (20),
+primary key (id)
+);
+
+ insert into  medico values (null,'333-3','Camila','Gonzalez');
+ insert into  medico values(null,'444-4','Valeria','Flores');
+ insert into  medico values (null,'555-5','Pedro','Fernandez');
+ insert into  medico values (null,'666-6','Joaquin','Olivares');
+ insert into  medico values (null,'777-7','Mauricio','Castillo');
+ insert into  medico values (null,'888-8','Isidora','Castro');
+ insert into  medico values (null,'999-9','Dania','Pereira');
+
+ create table administrador (
+	id int auto_increment,
+    rut varchar(12),
+    clave varchar (25),
+    primary key (id)
+);
+insert into administrador values (null,'111-1','123456');
+
+create table atencion(
+	id int auto_increment,
+    idMascota int,
+    idMedico int,
+    primary key (id),
+    foreign key (idMascota) references mascota (id),
+    foreign key (idMedico) references medico (id)
+);
+
+insert into atencion values (null,2,4);
+
+
+select * from mascota;
+
+ select m.id, m.nombre, m.edad, m.raza, m.sexo, m.vacunas, p.nombre as 'propietario'
+                from mascota m, propietario p;
+                /*where m.id = 2;*/
+
+select * from mascota;
+select * from medico;
+select * from atencion;
+
+select m.nombre as 'Mascota', m.edad, m.raza, m.sexo, m.vacunas, me.nombre 'Medico', a.id 'Numero Atencion'
+	from mascota m, medico me, atencion a
+    where m.id = 2 and me.id = 4;
