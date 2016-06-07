649bd2045bab3484f6e606c7cecf3a6fe54579cf
Gonzalo Zuñiga <astaroth_x3m@hotmail.com>
2016-06-07 12:30:14 -0300

    Mejoras en aspecto y orden de Index_V..1.02

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta charset="utf-8">
     <title>Inicio de Sesión</title>
+    <style media="screen">
+      body{
+        background-image: url("img\\banner2.jpg");
+      }
+    </style>
   </head>
   <body>
     <div class="panelForm">
