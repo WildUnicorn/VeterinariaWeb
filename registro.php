fd6f63a94833fe7307041ab2efea9dfc90aa739e
Gonzalo Zuñiga <astaroth_x3m@hotmail.com>
2016-06-07 13:46:39 -0300

    Registro final

--- registro.php
+++ registro.php
@@ -5,7 +5,33 @@
 <html>
   <head>
     <meta charset="utf-8">
+
     <title>Registro</title>
+    <style media="screen">
+    body{
+      background-image: url("img/banner1.jpg");
+      background-repeat: no-repeat;
+      background-position:top center;
+    }
+    table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+
+
+
+    </style>
   </head>
   <body>
 
@@ -16,7 +42,6 @@
       $action = "controlador/resgistrarUsuario.php";
           $d->formularioCrear($action);
        ?>
-
 
   </body>
 </html>
